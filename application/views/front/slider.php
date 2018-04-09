f71d1cfb020b80e1ac626565b86ae18afa163233
agrofresh123 <freshagroindia@gmail.com>
2018-04-09 13:58:10 +0530

    Updated..
    Updated..

--- application/views/front/slider.php
+++ application/views/front/slider.php
@@ -1,53 +1,100 @@
-<section class="banner-slit demo-2 clearfix">
-  <div id="slider" class="sl-slider-wrapper">
-    <div class="sl-slider">
-      <div class="sl-slide sl-slide-horizontal" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
-        <div class="sl-slide-inner">
-          <div class="bg-img bg-img-1"></div>
-          <div class="ban-cotent">
-            <div class="ban1-left"> <span class="ban1-txt1"> High Quality Care when </span><br>
-              <span class="ban1-txt2"> Timing is everything </span><br>
-              <span class="ban1-txt3"> Donec libero dui, dapibus non leo et, molestie faucibus<br>
-            risus. In fermentum tortor et posuere laoreet. </span><br>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="sl-slide sl-slide-verticle" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
-        <div class="sl-slide-inner">
-          <div class="bg-img bg-img-2"></div>
-          <div class="ban-cotent">
-            <div class="ban2-left"> <span class="ban1-txt1"> Expert care delivered </span><br>
-              <span class="ban2-txt2"> BY OUR SPECIALIST </span><br>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="sl-slide sl-slide-horizontal" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
-        <div class="sl-slide-inner">
-          <div class="bg-img bg-img-1"></div>
-          <div class="ban-cotent">
-            <div class="ban1-left"> <span class="ban1-txt1"> High Quality Care when </span><br>
-              <span class="ban1-txt2"> Timing is everything </span><br>
-              <span class="ban1-txt3"> Donec libero dui, dapibus non leo et, molestie faucibus<br>
-            risus. In fermentum tortor et posuere laoreet. </span><br>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="sl-slide sl-slide-verticle" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
-        <div class="sl-slide-inner">
-          <div class="bg-img bg-img-2"></div>
-          <div class="ban-cotent">
-            <div class="ban2-left"> <span class="ban1-txt1"> Expert care delivered </span><br>
-              <span class="ban2-txt2"> BY OUR SPECIALIST </span><br>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /sl-slider -->
-    <nav id="nav-arrows" class="nav-arrows"> <span class="nav-arrow-prev">Previous</span> <span class="nav-arrow-next">Next</span> </nav>
-  </div>
-  <!-- /slider-wrapper -->
-</section>
+<section class="banner-slit demo-2 clearfix" style="display:none;">
+  <div id="slider" class="sl-slider-wrapper">
+    <div class="sl-slider">
+      <div class="sl-slide sl-slide-horizontal" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+        <div class="sl-slide-inner">
+          <div class="bg-img bg-img-1"></div>
+          <div class="ban-cotent">
+            <div class="ban1-left"> <span class="ban1-txt1"> High Quality Care when </span><br>
+              <span class="ban1-txt2"> Timing is everything </span><br>
+              <span class="ban1-txt3"> Donec libero dui, dapibus non leo et, molestie faucibus<br>
+              risus. In fermentum tortor et posuere laoreet. </span><br>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sl-slide sl-slide-verticle" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+        <div class="sl-slide-inner">
+          <div class="bg-img bg-img-2"></div>
+          <div class="ban-cotent">
+            <div class="ban2-left"> <span class="ban1-txt1"> Expert care delivered </span><br>
+              <span class="ban2-txt2"> BY OUR SPECIALIST </span><br>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sl-slide sl-slide-horizontal" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+        <div class="sl-slide-inner">
+          <div class="bg-img bg-img-1"></div>
+          <div class="ban-cotent">
+            <div class="ban1-left"> <span class="ban1-txt1"> High Quality Care when </span><br>
+              <span class="ban1-txt2"> Timing is everything </span><br>
+              <span class="ban1-txt3"> Donec libero dui, dapibus non leo et, molestie faucibus<br>
+              risus. In fermentum tortor et posuere laoreet. </span><br>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sl-slide sl-slide-verticle" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+        <div class="sl-slide-inner">
+          <div class="bg-img bg-img-2"></div>
+          <div class="ban-cotent">
+            <div class="ban2-left"> <span class="ban1-txt1"> Expert care delivered </span><br>
+              <span class="ban2-txt2"> BY OUR SPECIALIST </span><br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /sl-slider -->
+    <nav id="nav-arrows" class="nav-arrows"> <span class="nav-arrow-prev">Previous</span> <span class="nav-arrow-next">Next</span> </nav>
+  </div>
+  <!-- /slider-wrapper -->
+</section>
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="assets/images/ban-plain1.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="assets/images/ban-plain2.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="assets/images/ban-plain3.jpg" alt="New York">
+    </div>
+    
+    <div class="item">
+      <img src="assets/images/ban-plain4.jpg" alt="New York">
+    </div>
+    
+    <div class="item">
+      <img src="assets/images/ban-plain5.jpg" alt="New York">
+    </div>
+    
+    <div class="item">
+      <img src="assets/images/ban-plain6.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
